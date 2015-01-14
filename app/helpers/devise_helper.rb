bef55e0e2f9a50b3b5f219d9cf149fd5032d6665
anishbharatha <anish.bharatha@gmail.com>
2015-01-13 23:38:38 -0600

    sendgrid added

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -10,4 +10,12 @@ module DeviseHelper
     resource.errors.empty? ? false : true
   end
 
+  def send_email
+    sendgrid_api = YAML.load_file("#{Rails.root.to_s}/config/secretkeys.yml")[Rails.env]
+    # As a hash
+    client = SendGrid::Client.new(api_user: ''+sendgrid_api['sendgrid_api']['username'], api_key: ''+sendgrid_api['sendgrid_api']['key'])
+    client.send(SendGrid::Mail.new(to: 'anish.bharata@gmail.com', from: 'iReviewSchool@tester.in',
+                                   subject: 'Signed In Alert', text: 'Hi there! User Signed In: '+User.current.public_name.to_s,
+                                   html: 'Hi there! User Signed In: '+User.current.public_name.to_s))
+  end
 end
