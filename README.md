693c359463d6ea08504207e8d994f6abc22c3295
anishbharatha <anish.bharatha@gmail.com>
2015-01-20 23:49:51 -0600

    for heroku

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 schoolReview
 ============
 
-Forums-like platform to read &amp; review schools/colleges/any other educational institution anonymously
+Forums-like platform to read &amp; review schools/colleges/any other educational institution anonymously.
+
+Prerequisites:
+    1. mysqlclient, libmagickwand-dev
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SchoolReview::Application.config.secret_key_base = '3b8af16a018b8f2a253aaa53da3a97999b7bc079b13d7371a431af719489a78dd574210144b263a03cf0418fee2fccab11516c111a1a456238948b508006e27f'
