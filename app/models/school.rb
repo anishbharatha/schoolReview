183b64ef6b9f478d6274cd50f48c247a3873b77c
anishbharatha <anish.bharatha@gmail.com>
2015-03-15 00:56:33 -0700

    Elasticsearch/ELK stack implemented

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,17 +1,8 @@
 class School < ActiveRecord::Base
   has_many :reviews
   belongs_to :user
+
+  searchkick
+
   mount_uploader :school_logo, SchoolLogoUploader
-
-  def self.search(search)
-    if search
-      search_terms  = search.to_s.split(/[^0-9A-Za-z]/) #Splitting whole string
-      search_terms.delete(' ') #Removing any empty strings
-      Rails.logger.debug '[SEARCH TERMS 1+] '+search_terms.to_s
-      where('aka LIKE ? OR aka LIKE ?',search_terms, search_terms)
-    else
-      all
-    end
-  end
-
 end
